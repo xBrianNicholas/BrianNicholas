062b736fdd5a9eaad152feb19517d95e62cb78b7
Brian <20256029@student.curtin.edu.au>
2020-04-02 16:01:07 +0400

    Update in GridViewer.java

---
+++ GridViewer.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Maze
+{
+   public static void view(int[][] grid)
+   {         
+        for(int i = 0; i < grid.length; i++)
+        {
+            for(int j = 0; j < grid[i].length; j++)
+            {
+                switch(grid[i][j])
+                {
+                    case Maze.EMPTY:
+                        System.out.print("  ");
+                        break;
+                        
+                    case Maze.WALL:
+                        System.out.print("##");
+                        break;
+                        
+                    case Maze.START:
+                        System.out.print("^^");
+                        break;
+                        
+                    case Maze.END:
+                        System.out.print("$$");
+                        break;
+                        
+                    case Maze.VISITED:
+                        System.out.print("..");
+                        break;
+                        
+                    default:
+                        throw new AssertionError();
+                }
+            }
+            System.out.println();
+        }
+   }
+}
