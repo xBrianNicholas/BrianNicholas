4cbef360b2082eb329bbc04dbf3802440920336e
Brian <20256029@student.curtin.edu.au>
2020-04-02 16:00:00 +0400

    Update in Maze.java

--- Maze.java
+++ Maze.java
@@ -26,38 +26,7 @@ public class Maze
             { WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL, WALL},
         };
                 
-        for(int i = 0; i < grid.length; i++)
-        {
-            for(int j = 0; j < grid[i].length; j++)
-            {
-                switch(grid[i][j])
-                {
-                    case EMPTY:
-                        System.out.print("  ");
-                        break;
-                        
-                    case WALL:
-                        System.out.print("##");
-                        break;
-                        
-                    case START:
-                        System.out.print("^^");
-                        break;
-                        
-                    case END:
-                        System.out.print("$$");
-                        break;
-                        
-                    case VISITED:
-                        System.out.print("..");
-                        break;
-                        
-                    default:
-                        throw new AssertionError();
-                }
-            }
-            System.out.println();
-        }
+       GridViewer.view(grid);
         
         String solution;
         Scanner sc = new Scanner(System.in);
@@ -137,37 +106,7 @@ public class Maze
             System.out.println("MESSAGE 5"); // Did not reach the end.
         }
         
-        for(int i = 0; i < grid.length; i++)
-        {
-            for(int j = 0; j < grid[i].length; j++)
-            {
-                switch(grid[i][j])
-                {
-                    case EMPTY:
-                        System.out.print("  ");
-                        break;
-                        
-                    case WALL:
-                        System.out.print("##");
-                        break;
-                        
-                    case START:
-                        System.out.print("^^");
-                        break;
-                        
-                    case END:
-                        System.out.print("$$");
-                        break;
-                        
-                    case VISITED:
-                        System.out.print("..");
-                        break;
-                        
-                    default:
-                        throw new AssertionError();
-                }
-            }
-            System.out.println();
+       GridViewer.view(grid);
         }
     }
 }
