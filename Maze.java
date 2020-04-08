788244f06ce5c21b8006895b35a0ee6a1e099875
Brian <20256029@student.curtin.edu.au>
2020-04-08 23:47:41 +0400

    rename_vars changes updated

--- Maze.java
+++ Maze.java
@@ -79,16 +79,16 @@ public class Maze
             }
             else
             {
-                if(grid[currentRow][currentCol] == EMPTY)
+                if(grid[row][col] == EMPTY)
                 {
-                    grid[currentRow][currentCol] = VISITED;
+                    grid[row][col] = VISITED;
                 }
-                else if(grid[currentRow][currentCol] == WALL)
+                else if(grid[row][col] == WALL)
                 {
                     done = true;
                     System.out.println("You stumble blindly into a solid concrete wall."); // Hit wall.
                 }
-                else if(grid[currentRow][currentCol] == END)
+                else if(grid[row][col] == END)
                 {
                     done = true;
                     solved = true;
